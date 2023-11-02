0178da58c092cff3a561bfb5bb1dfc8ff377c7a6
Daniel Lima <deltaalfalambda@gmail.com>
2023-11-01 23:16:16 -0300

    Iniciando refatoração da conexao

--- core/Database.php
+++ core/Database.php
@@ -1,2 +1,22 @@
 <?php
 
+class Database{
+    static protected $conn;
+
+    public function getConexao(){
+        if(Database::conn){
+            return Database::conn;
+        }
+
+        try {
+            $this->conn = new PDO("mysql:host=".DATABASE_HOST.";".
+                "dbname=".DATABASE_NAME, 
+                DATABASE_USER, 
+                DATABASE_PASS);
+            
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro na conexão com o banco de dados: " . $e->getMessage());
+        }
+    }
+}
